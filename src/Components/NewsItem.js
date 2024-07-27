0579371cfa22edf069b63c20dc684bf52b607097
anayatur1 <119777828+anayatur1@users.noreply.github.com>
2024-07-27 16:13:24 +0530

    Add files via upload

---
+++ src/Components/NewsItem.js
@@ -0,0 +1,33 @@
+import React from 'react'
+
+const NewsItem=(props)=>{
+
+    let {title,description,imageUrl,newsUrl,author,date,source} = props;
+    return (
+    <>
+    <div   className="card" style={{width: "18rem"}}>
+  <img src={
+    !imageUrl ? "https://static.vecteezy.com/system/resources/thumbnails/003/778/434/small/breaking-news-background-tv-channel-news-screensaver-illustration-free-vector.jpg":imageUrl }  
+  className="card-img-top" alt="..."/>
+  <div   className="card-body">
+    <h5   className="card-title">{title}...</h5>
+    <span className="position-absolute top-0  translate-middle badge rounded-pill bg-danger"
+     style={{left: '92%',zIndex:'1'}}>
+    {source}
+    <span className="visually-hidden">unread messages</span>
+  </span>
+    <p   className="card-text"> {description}...</p>
+    <p className="card-text"><small className="text-muted">
+      By {!author?"unknown":author} on {new Date(date).toGMTString()}</small></p>
+    <a href={newsUrl}
+    rel="noreferrer"
+     target="_blank"   className="btn btn-sm btn-primary">
+      Read More
+        </a>
+  </div>
+</div>
+    </>
+    )
+  }
+
+export default NewsItem
